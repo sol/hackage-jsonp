9b85b8080f4c764d0d11ba855ac006bc14116bf6
Simon Hengel <sol@typeful.net>
2012-03-24 15:51:43 +0100

    Add parseMany

--- Main.hs
+++ Main.hs
@@ -1,9 +1,29 @@
 module Main where
 
+import           Data.Map   (Map)
+import qualified Data.Map as Map
+import qualified Data.ByteString.Lazy as LB
+
+import           Data.Aeson.Generic
+
 import           Package hiding (name, version)
 import qualified Package
 
-parse :: String -> Either String Package
+parse :: String -> (String, String)
 parse input = case (reverse . words) input of
-  version : name : _ -> Right (Package name version)
-  _                  -> Left ("invalid input: " ++ show input)
+  version : name : _ -> (name, version)
+  _                  -> error ("invalid input: " ++ show input)
+
+parseMany :: String -> Map String String
+parseMany input = foldr f Map.empty (lines input)
+  where
+    f s = Map.alter g name
+      where
+        g (Just old) | version < old = Just old
+        g _                          = Just version
+        (name, version) = parse s
+
+main = do
+  input <- readFile "log"
+  let packages = parseMany input
+  LB.writeFile "log.json" (encode packages)
--- Spec.hs
+++ Spec.hs
@@ -2,9 +2,13 @@
 module Spec (main, spec) where
 
 import           Test.Hspec.ShouldBe
+import           Data.String.Builder
 
-import           Data.Aeson.Generic
+import           Data.Map   (Map)
+import qualified Data.Map as Map
 import           Data.ByteString.Lazy.Char8 ()
+import           Data.Aeson.Generic
+
 import           Package
 import           Main hiding (main)
 
@@ -17,6 +21,25 @@ spec = do
     it "converts a Package to JSON" $ do
       encode (Package "hspec" "0.9.2") `shouldBe` "{\"name\":\"hspec\",\"version\":\"0.9.2\"}"
 
+    it "converts a package index to JSON" $ do
+      encode (Map.fromList [("foo", "0.1"), ("bar", "0.2"), ("baz", "0.3")] :: Map String String)
+        `shouldBe` "{\"foo\":\"0.1\",\"bar\":\"0.2\",\"baz\":\"0.3\"}"
+
   describe "parse" $ do
     it "parses a Package" $ do
-      parse "Sun Mar 18 05:12:30 UTC 2012 SimonHengel hspec 0.9.2" `shouldBe` Right (Package "hspec" "0.9.2")
+      parse "Sun Mar 18 05:12:30 UTC 2012 SimonHengel hspec 0.9.2" `shouldBe` ("hspec", "0.9.2")
+
+  describe "parseMany" $ do
+    it "parses lines from Hackage upload log" $ do
+      parseMany . build $ do
+        "Sun Mar 18 05:12:30 UTC 2012 SimonHengel foo 0.1"
+        "Sun Mar 18 05:12:30 UTC 2012 SimonHengel bar 0.2"
+        "Sun Mar 18 05:12:30 UTC 2012 SimonHengel baz 0.3"
+      `shouldBe` Map.fromList [("foo", "0.1"), ("bar", "0.2"), ("baz", "0.3")]
+
+    it "gives package with higher version number precedence" $ do
+      parseMany . build $ do
+        "Sun Mar 18 05:12:30 UTC 2012 SimonHengel hspec 0.0.2"
+        "Sun Mar 18 05:12:30 UTC 2012 SimonHengel hspec 0.9.2"
+        "Sun Mar 18 05:12:30 UTC 2012 SimonHengel hspec 0.0.3"
+      `shouldBe` Map.fromList [("hspec", "0.9.2")]
